65c886ba53dc998fd51ce3174b45ed7f5e828133
MadeFix <oladosulekan@yahoo.com>
2019-03-14 07:59:02 +0100

    - Added test cases for movie model

--- StreamsTests/StreamsTests.swift
+++ StreamsTests/StreamsTests.swift
@@ -30,5 +30,22 @@ class StreamsTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    // MARK: Movie Model Class Tests
+    
+    // Confirm that the model init correctely and return an object when passed
+    func testMovie() {
+        let movie = Movie.init(title: "Title", year: "", rated: "", released: "", runtime: "", genre: "", director: "", writer: "", actors: "", plot: "", language: "", country: "", awards: "", poster: "", ratings: [], metascore: "", imdbRating: "", imdbVotes: "", imdbID: "", type: "", dvd: "", boxOffice: "", production: "", website: "", response: "")
+        XCTAssertNotNil(movie)
+    }
+    
+    // Confirm that a title cannot be empty
+    func testMovieTitleNotEmpty() {
+        // In a standard we will put a check in the movie class to reject empty title from being created as a movie
+        let movie = Movie.init(title: "", year: "", rated: "", released: "", runtime: "", genre: "", director: "", writer: "", actors: "", plot: "", language: "", country: "", awards: "", poster: "", ratings: [], metascore: "", imdbRating: "", imdbVotes: "", imdbID: "", type: "", dvd: "", boxOffice: "", production: "", website: "", response: "")
+        
+        XCTAssertNotEqual(movie.title, "")
+        
+    }
 
 }
